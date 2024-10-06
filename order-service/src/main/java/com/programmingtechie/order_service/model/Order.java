21d5ddebd2751e076e954098b4c051e978c7fe3e
Akash <akashk.bdvt1@gmail.com>
2024-10-06 19:49:59 +0530

    first commit

---
+++ order-service/src/main/java/com/programmingtechie/order_service/model/Order.java
@@ -0,0 +1,24 @@
+package com.programmingtechie.order_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItems;
+
+}
---
+++ order-service/src/main/java/com/programmingtechie/order_service/model/OrderLineItems.java
@@ -0,0 +1,22 @@
+package com.programmingtechie.order_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Long price;
+    private Integer quantity;
+}
